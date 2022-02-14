fd92588ef5407091994f3b3bec653b3db5125be4
Piotr <straszak.piotr@gmail.com>
2022-02-14 12:07:51 +0100

    Section 22: @Output()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,8 @@ import { Quotation } from './models/quotation';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showForm: boolean = false;
   quotations: Quotation[] = QUOTES;
   quotation: Quotation = {author: '', sentence: '', votes: 0}
-
-  toggleForm() {
-    this.showForm = !this.showForm
-  }
-
-  addQuotation() {
-    this.quotations.unshift(this.quotation)
-    this.quotation = {author: '', sentence: '', votes: 0}
-  }
 
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value
@@ -31,5 +21,9 @@ export class AppComponent {
 
   worstQuotations() {
     return this.quotations.sort( (a, b) => { return a.votes - b.votes } ).slice(0, 3)
+  }
+
+  onNewQuotation(quotation) {
+    this.quotations.unshift(quotation)
   }
 }
