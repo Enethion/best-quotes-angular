384c6bb97bfac2ec96c2fbfbcac73afdb3f8e65d
Piotr <straszak.piotr@gmail.com>
2022-02-14 09:22:49 +0100

    Section 19: The beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/data-base';
+import { Quotation } from './models/quotation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm: boolean = false;
+  quotations: Quotation[] = QUOTES;
+  quotation: Quotation = {author: '', sentence: '', votes: 0}
+
+  toggleForm() {
+    this.showForm = !this.showForm
+  }
+
+  addQuotation() {
+    this.quotations.unshift(this.quotation)
+    this.quotation = {author: '', sentence: '', votes: 0}
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value
+  }
+}
